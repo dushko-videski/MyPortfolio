543a2dafc16c1951bf3bc7a49e0f9bb2a1c331db
Dushko Videski <dushko@codechem.com>
2021-06-26 23:16:25 +0200

    initial change

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,10 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
             Hi, I'm Dushko.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />I am a Full Stack Web Developer working with .NET Core in combination with React JS and Microsoft SQL Server. 
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          Experienced Senior Loan Officer with a demonstrated history of working in the banking industry.
           </p>
           <div className="flex justify-center">
             <a
